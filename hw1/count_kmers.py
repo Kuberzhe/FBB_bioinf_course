183bfb9abdea6031524989fecd9cc720b001a1bf
Kuberzhe <Kuberzhe@github.com>
2026-03-18 23:19:53 +0300

    added count_kmers.py

---
+++ hw1/count_kmers.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--fa", type=str, required=True, help="Input fasta file")
+args = parser.parse_args()
+
+k = 4
+kmers = {}
+cur_seq = ''
+
+
+with open(f'{args.fa}', 'r') as input:
+    for line in input:
+        if line.startswith('>'):
+            if cur_seq != '':
+                kmers[cur_seq_name] = {}
+                for i in range(len(cur_seq) - k):
+                    if cur_seq[i: i+k] not in kmers[cur_seq_name]:
+                        kmers[cur_seq_name][cur_seq[i: i+k]] = 1
+                    else:
+                        kmers[cur_seq_name][cur_seq[i: i + k]] += 1
+            cur_seq_name = line.strip()[1:]
+            cur_seq = ''
+        else:
+            line = line.strip()
+            cur_seq += line
+
+kmers[cur_seq_name] = {}
+
+for i in range(len(cur_seq) - k):
+    if cur_seq[i: i + k] not in kmers[cur_seq_name]:
+        kmers[cur_seq_name][cur_seq[i: i + k]] = 1
+    else:
+        kmers[cur_seq_name][cur_seq[i: i + k]] += 1
+
+with open("cnts.json", "w") as out:
+    json.dump(kmers, out, indent=3)
